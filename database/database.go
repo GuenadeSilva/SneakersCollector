e50b03358aca1e8321afb8f6432b852851fcb0c2
GuenadeSilva <guenade.silva@gmail.com>
2023-08-20 12:25:59 +0100

    Added logging functionality

--- database/database.go
+++ database/database.go
@@ -2,7 +2,9 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"time"
 
 	"sneakercollector/scrapper"
 
@@ -17,7 +19,7 @@ func RefreshScrapedData() {
 	}
 	defer db.Close()
 
-	// Create the table if it doesn't exist
+	// Create the sneaker_table if it doesn't exist
 	createTableStmt := `
 		CREATE TABLE IF NOT EXISTS sneaker_table (
 			id SERIAL PRIMARY KEY,
@@ -32,49 +34,68 @@ func RefreshScrapedData() {
 		return
 	}
 
-	_, err = db.Exec("DELETE FROM sneaker_table")
+	createLogTableStmt := `
+	CREATE TABLE IF NOT EXISTS log_table (
+		id SERIAL PRIMARY KEY,
+		message TEXT,
+		timestamp TIMESTAMP
+	)
+`
+	_, err = db.Exec(createLogTableStmt)
 	if err != nil {
-		log.Printf("Error deleting existing data: %v", err)
+		log.Printf("Failed to create log table: %v", err)
 		return
 	}
 
-	// Scrape data from Adidas
-	adidasShoes := scrapper.ScrapeProducts("Adidas")
-	for _, shoe := range adidasShoes {
-		_, err = db.Exec("INSERT INTO sneaker_table (name, price, link) VALUES ($1, $2, $3)",
-			shoe.NAME, shoe.PRICE, shoe.LINK)
+	// Function to log each step
+	logStep := func(operation, brand string, startTime time.Time, rowsInserted int, success bool) {
+		endTime := time.Now()
+		elapsedTime := endTime.Sub(startTime)
+		status := "successful"
+		if !success {
+			status = "failed"
+		}
+		logMessage := fmt.Sprintf("Brand: %s\n", brand)
+		logMessage += fmt.Sprintf("Operation: %s\n", operation)
+		logMessage += fmt.Sprintf("Status: %s\n", status)
+		logMessage += fmt.Sprintf("Time taken: %s\n", elapsedTime)
+		logMessage += fmt.Sprintf("Rows inserted: %d\n", rowsInserted)
+		logMessage += fmt.Sprintf("Timestamp: %s\n", endTime)
+		logMessage += "---------------------------\n"
+
+		// Insert the log into log_table
+		_, err = db.Exec("INSERT INTO log_table (message, timestamp) VALUES ($1, $2)",
+			logMessage, time.Now())
 		if err != nil {
-			log.Printf("Error inserting Adidas data: %v", err)
-			return
+			log.Printf("Error inserting log into log_table: %v", err)
 		}
 	}
 
-	// Scrape data from New Balance
-	nbShoes := scrapper.ScrapeProducts("New Balance")
-	for _, shoe := range nbShoes {
-		_, err = db.Exec("INSERT INTO sneaker_table (name, price, link) VALUES ($1, $2, $3)",
-			shoe.NAME, shoe.PRICE, shoe.LINK)
-		if err != nil {
-			log.Printf("Error inserting New Balance data: %v", err)
-			return
+	// Scrape and insert data for different brands
+	scrapeAndInsert := func(brand string) {
+		startTime := time.Now()
+		var shoes []scrapper.ShoeInfo
+		if brand == "Nike" {
+			shoes, _ = scrapper.ScrapeProductsx(false)
+		} else {
+			shoes = scrapper.ScrapeProducts(brand)
 		}
-	}
-
-	// Scrape data from Nike
-	newShoes, err := scrapper.ScrapeProductsx(false)
-	if err != nil {
-		log.Printf("Error scraping new data: %v", err)
-		return
-	}
-
-	for _, shoe := range newShoes {
-		_, err = db.Exec("INSERT INTO sneaker_table (name, price, link) VALUES ($1, $2, $3)",
-			shoe.NAME, shoe.PRICE, shoe.LINK)
-		if err != nil {
-			log.Printf("Error inserting new data: %v", err)
-			return
+		rowsInserted := 0
+		for _, shoe := range shoes {
+			_, err = db.Exec("INSERT INTO sneaker_table (name, price, link) VALUES ($1, $2, $3)",
+				shoe.NAME, shoe.PRICE, shoe.LINK)
+			if err == nil {
+				rowsInserted++
+			}
 		}
+		logStep(fmt.Sprintf("ScrapeProducts('%s')", brand), brand, startTime, rowsInserted, err == nil)
 	}
 
-	log.Println("Scraped data refreshed successfully")
+	// Scrape data for different brands
+	brands := []string{"Adidas", "New Balance", "Nike"}
+	for _, brand := range brands {
+		scrapeAndInsert(brand)
+	}
+
+	log.Println("Scraped data refresh completed")
 }
